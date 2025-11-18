6ab735773334730de89dacacfc1cdc63b7383d01
aritra0609 <aritraganguly2019@gmail.com>
2025-11-18 13:42:03 +0530

    Enhance chat endpoint with error handling
    Refactor chat endpoint to handle exceptions and improve response handling.

--- backend.py
+++ backend.py
@@ -1,36 +1,37 @@
-#Step1: Setup pydantic model (schema validation)
+# backend.py
+from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+
+from ai_agent import get_response_from_ai_agent
+
+ALLOWED_MODEL_NAMES = ["qwen/qwen3-32b", "llama-3.3-70b-versatile"]
+
 class RequestState(BaseModel):
     model_name: str
     model_provider: str
     system_prompt: str
     messages: List[str]
     allow_search: bool
-#Step2: Setup AI Agent from Frontend Request
-from fastapi import FastAPI
-from ai_agent import get_response_from_ai_agent 
-ALLOWED_MODEL_NAMES=["llama3-70b-8192", "mistral-saba-24b", "llama-3.3-70b-versatile"]
-app=FastAPI(title="Langgraph AI Agent")
+
+app = FastAPI(title="Langgraph AI Agent")
+
 @app.post("/chat")
 def chat_endpoint(request: RequestState):
-    """
-    API endpoint to interact with Chatbot using Langgraph and search tools.
-    It dynamically selects the model specified in the request
-    """
     if request.model_name not in ALLOWED_MODEL_NAMES:
         return {"error": "Model not found"}
-    llm_id=request.model_name
-    query=request.messages
-    allow_search=request.allow_search
-    system_prompt=request.system_prompt
-    provider=request.model_provider
+    try:
+        resp = get_response_from_ai_agent(
+            llm_id=request.model_name,
+            messages=request.messages,
+            allow_search=request.allow_search,
+            system_prompt=request.system_prompt,
+            provider=request.model_provider
+        )
+        return {"response": resp}
+    except Exception as e:
+        return {"error": str(e)}
 
-    #Create AI Agent and get response from it
-    response=get_response_from_ai_agent(llm_id, query, allow_search, system_prompt, provider)
-    return response
-#Step3: Run app
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=10000)
-
+    uvicorn.run(app, host="127.0.0.1", port=9999)
